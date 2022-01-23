159594b210db10428af8b93270a8aeff68103690
LemonJoey <JosephRedmond@Hotmail.co.uk>
2022-01-23 02:11:03 +0000

    Finished

--- src/main/java/com/live/lemonjoey/shopwarp/Events.java
+++ src/main/java/com/live/lemonjoey/shopwarp/Events.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.Objects;
+import java.util.Random;
 
 import static com.live.lemonjoey.shopwarp.Main.plugin;
 
@@ -19,11 +20,25 @@ public class Events implements Listener {
         if (!event.getAction().equals(Action.PHYSICAL)) return;
         if (Objects.requireNonNull(event.getClickedBlock()).getType() != Material.STONE_PRESSURE_PLATE) return;
         if (clicker.getLocation().distance(new Location(plugin.getServer().getWorld("event"), 0, 48, -437)) < 2) {
-            for (Player player : plugin.getServer().getOnlinePlayers()) {
-                if (player.hasPermission("ShopWarp.debug"))
-                    player.sendMessage(String.format("testing", clicker
-                            .getDisplayName()));
+            float minX = 11;
+            float maxX = 15;
+            float minZ = -435;
+            float maxZ = -259;
+            float minYAW = -180;
+            float maxYAW = 180;
+
+            if (new Random().nextBoolean()){
+                minX -=24;
+                maxX -=24;
             }
+
+            float[] result=new float[4];
+            result[0]= (float) (Math.random()*(maxX-minX)+minX);
+            result[1]=50;
+            result[2]= (float) (Math.random()*(maxZ-minZ)+minZ);
+            result[3]= (float) (Math.random()*(maxYAW-minYAW)+minYAW);
+
+            clicker.teleport(new Location(event.getPlayer().getWorld(), result[0], result[1], result[2], result[3], 0));
         }
     }
 }
